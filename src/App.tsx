243006ac7e53b9b580fa819eebbdaa060d56773b
Alaa-Hussein002 <ala.hussein002@gmail.com>
2026-09-07 22:33:22 +0300

    feat: remove SEO metadata from index.html and SeoHead component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { I18nProvider } from "./i18n";
 import { ThemeProvider } from "./contexts/ThemeContext"; // ✅ إضافة
 import { AppRouter } from "./router";
-import { SeoHead } from "./components/layout/SeoHead";
 
 export default function App() {
   return (
     <ThemeProvider> {/* ✅ إضافة */}
       <I18nProvider>
-        <SeoHead />
         <AppRouter />
       </I18nProvider>
     </ThemeProvider>
